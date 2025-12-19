8a36a38fe9823e0724a36253c1d6753feda07576
Aniket <aniketindian8@gmail.com>
2025-12-20 03:00:06 +0530

    Decoder for NN

---
+++ ml/src/main/java/aniket762/combinehealth/nn/TransformerDecoder.java
@@ -0,0 +1,31 @@
+package aniket762.combinehealth.nn;
+
+import aniket762.combinehealth.core.Matrix;
+import aniket762.combinehealth.core.MatrixOps;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransformerDecoder {
+    private final Embedding embedding;
+    private final List<DecoderBlock> blocks;
+    private final Matrix lmHead;
+
+    public TransformerDecoder(int vocab, int layers, int dModel, int heads,int dff){
+        embedding = new Embedding(vocab,dModel);
+        blocks = new ArrayList<>();
+        for(int i=0;i<layers;i++){
+            blocks.add(new DecoderBlock(dModel,heads,dff));
+        }
+        lmHead = Matrix.random(dModel,vocab);
+    }
+
+    public Matrix forward(int[] tokens){
+        Matrix x = embedding.forward(tokens);
+        PositionalEncoding.add(x);
+
+        for(DecoderBlock b: blocks) x = b.forward(x);
+
+        return MatrixOps.matmul(x,lmHead);
+    }
+}
