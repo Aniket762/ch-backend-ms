2df32ab36d89f7c72077e7525b3897a08b52431c
Aniket <aniketindian8@gmail.com>
2025-12-20 14:36:01 +0530

    Config: Swagger + CORS for REST End Point Access

--- src/main/java/aniket762/combinehealth/controller/TrainingController.java
+++ src/main/java/aniket762/combinehealth/controller/TrainingController.java
@@ -2,10 +2,13 @@ package aniket762.combinehealth.controller;
 
 import aniket762.combinehealth.service.ModelService;
 import aniket762.combinehealth.service.TrainingStatus;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@Tag(name="Train Model", description = "Train Model and Fetch Status of Training")
 @RestController
 @RequestMapping("/api/model")
 public class TrainingController {
@@ -14,6 +17,7 @@ public class TrainingController {
         this.modelService = modelService;
     }
 
+    @Operation(summary = "train model")
     @PostMapping("/train")
     public Map<String,String> train(@RequestBody Map<String,String> body) throws Exception {
         String url = body.get("url");
@@ -21,6 +25,7 @@ public class TrainingController {
         return Map.of("status","training_started");
     }
 
+    @Operation(summary = "get status")
     @GetMapping("/status")
     public Map<String,String> status(){
         TrainingStatus status = modelService.getStatus();
